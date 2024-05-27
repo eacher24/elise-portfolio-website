400748c1787c82e9d5f087956496f626fd521669
Elise Acher eliseacher729@gmail.com <eliseacher729@gmail.com>
2024-05-27 16:38:30 +0100

    creating create functional components our of the individual page views

---
+++ src/components/Landing.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div>
+        <div class="quote-container">
+          <blockquote class="quote">
+            "When tech meets creativity, the impossible starts to become...
+            <div class="typewriter">
+              <p class="achievable">ACHIEVABLE"</p>
+            </div>
+          </blockquote>
+        </div>
+
+        <div class="more-button-container">
+            <a href="">Find Out More</a>
+        </div>
+    </div>
+
+  )
+}
+
+export default Landing
